54507be9bf316682604f0035eecbc792cb0a4ca5
Bruno M. Souza <brunomsouz@gmail.com>
2024-12-15 23:59:46 -0300

    Initial setup

---
+++ src/test/java/io/souz/fileingestionapi/FileIngestionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.souz.fileingestionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileIngestionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
